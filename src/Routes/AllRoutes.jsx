aa099745aa42d424ce1ea052d95cc214182d199e
G-Dharani-raj <105777295+G-Dharani-raj@users.noreply.github.com>
2023-01-17 18:18:04 +0530

    added Routes

---
+++ src/Routes/AllRoutes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+const AllRoutes = () => {
+	return (
+		<Routes>
+			<Route path="/" element={<h1>123</h1>} />
+			<Route path="/products" element={<h1>123</h1>} />
+			<Route path="/product_detail" element={<h1>123</h1>} />
+			<Route path="/cart" element={<h1>123</h1>} />
+			<Route path="/payment" element={<h1>123</h1>} />
+			<Route path="/checkout" element={<h1>123</h1>} />
+			<Route path="*" element={<h1>123</h1>} />
+		</Routes>
+	);
+};
+
+export default AllRoutes;

6cddc8d2c1901703fd614faa1498873c2fe15ade
G-Dharani-raj <105777295+G-Dharani-raj@users.noreply.github.com>
2023-01-19 20:32:44 +0530

    deleted a dup route

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -7,20 +7,19 @@ import HomePage from "../Components/HomePage/HomePage";
 import ProductDetails from "../Components/ProductPage/ProductDetails";
 import Products from "../Components/ProductPage/Products";
 const AllRoutes = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/products" element={<Products />} />
-      <Route path="/products/:id" element={<ProductDetails />} />
-      <Route path="/cart" element={<h1>123</h1>} />
-      <Route path="/payment" element={<h1>123</h1>} />
-      <Route path="/checkout" element={<h1>123</h1>} />
-      <Route path="/signup" element={<SignupCard />} />
-      <Route path="/checkout" element={<Checkout />} />
-      <Route path="/shipping" element={<Shippings />} />
-      <Route path="*" element={<h1>123</h1>} />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path="/" element={<HomePage />} />
+			<Route path="/products" element={<Products />} />
+			<Route path="/products/:id" element={<ProductDetails />} />
+			<Route path="/cart" element={<h1>123</h1>} />
+			<Route path="/payment" element={<h1>123</h1>} />
+			<Route path="/signup" element={<SignupCard />} />
+			<Route path="/checkout" element={<Checkout />} />
+			<Route path="/shipping" element={<Shippings />} />
+			<Route path="*" element={<h1>123</h1>} />
+		</Routes>
+	);
 };
 
 export default AllRoutes;

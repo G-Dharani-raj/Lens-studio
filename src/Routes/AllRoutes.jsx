c97dbea6586594b09137e3ddefa3b9401ac46c20
vaibhav waghmare <itsmevaibhavwaghmare7666@gmail.com>
2023-01-19 10:11:51 +0530

    adding day-1

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Products from "../Components/ProductPage/Products";
+import HomePage from "../Components/HomePage/HomePage";
+import ProductCard from "../Components/ProductPage/ProductCard";
 const AllRoutes = () => {
 	return (
 		<Routes>
-			<Route path="/" element={<h1>123</h1>} />
-			<Route path="/products" element={<h1>123</h1>} />
+			<Route path="/" element={ <HomePage/>} />
+			<Route path="/products" element={ <Products/>} />
 			<Route path="/product_detail" element={<h1>123</h1>} />
 			<Route path="/cart" element={<h1>123</h1>} />
 			<Route path="/payment" element={<h1>123</h1>} />

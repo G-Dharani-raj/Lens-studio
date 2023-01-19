dc3feddd594b08c6757f0f48df19825e4718faf3
Saurav903 <sk8517167@gmail.com>
2023-01-19 20:05:00 +0530

    changed the folder name

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -4,18 +4,18 @@ import Checkout from "../Components/Checkout/Checkout";
 import Shippings from "../Components/Checkout/Shippings";
 import HomePage from "../Components/HomePage/HomePage";
 const AllRoutes = () => {
-	return (
-		<Routes>
-			<Route path="/" element={<HomePage/>} />
-			<Route path="/products" element={<h1>123</h1>} />
-			<Route path="/product_detail" element={<h1>123</h1>} />
-			<Route path="/cart" element={<h1>123</h1>} />
-			<Route path="/payment" element={<h1>123</h1>} />
-			<Route path="/checkout" element={<Checkout/>} />
-			<Route path="/shipping" element={<Shippings/>} />
-			<Route path="*" element={<h1>123</h1>} />
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/products" element={<h1>1233</h1>} />
+      <Route path="/product_detail" element={<h1>123</h1>} />
+      <Route path="/cart" element={<h1>123</h1>} />
+      <Route path="/payment" element={<h1>123</h1>} />
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/shipping" element={<Shippings />} />
+      <Route path="*" element={<h1>123</h1>} />
+    </Routes>
+  );
 };
 
 export default AllRoutes;

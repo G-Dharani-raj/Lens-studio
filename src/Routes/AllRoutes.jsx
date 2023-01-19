6c5a777a012221d86c25c18b2f995444a16f5bfa
Saurav903 <sk8517167@gmail.com>
2023-01-19 20:25:42 +0530

    added routes

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import SignupCard from "../Cards/SignupCard";
 import Checkout from "../Components/Checkout/Checkout";
 import Shippings from "../Components/Checkout/Shippings";
 import HomePage from "../Components/HomePage/HomePage";
+import ProductDetails from "../Components/ProductPage/ProductDetails";
+import Products from "../Components/ProductPage/Products";
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/products" element={<h1>1233</h1>} />
-      <Route path="/product_detail" element={<h1>123</h1>} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/products/:id" element={<ProductDetails />} />
       <Route path="/cart" element={<h1>123</h1>} />
       <Route path="/payment" element={<h1>123</h1>} />
+      <Route path="/checkout" element={<h1>123</h1>} />
+      <Route path="/signup" element={<SignupCard />} />
       <Route path="/checkout" element={<Checkout />} />
       <Route path="/shipping" element={<Shippings />} />
       <Route path="*" element={<h1>123</h1>} />

ce2533f0d27052b21c8002b9e643e45a6c0f5e08
G-Dharani-raj <105777295+G-Dharani-raj@users.noreply.github.com>
2023-01-19 22:43:37 +0530

    removed the folder

---
+++ src/Components/Admin/Admin.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AdminNav from "./AdminNavbar/AdminNav";
+import AdminProducts from "./AdminProducts/AdminProducts";
+
+const Admin = () => {
+	return (
+		<div>
+			<AdminNav />
+			<AdminProducts />
+		</div>
+	);
+};
+
+export default Admin;

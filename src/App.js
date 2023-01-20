6be8e9d2baf3d29b6577abcc7fd407f116dc04d2
BaZuKaGanesh1 <ganesh12345bahire@gmail.com>
2023-01-20 10:33:13 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
-
+import Navbar from "./Components/Navbar/Navbar";
+// Please Dont Remove this line this is for CSS from BootStrap
+import "bootstrap/dist/css/bootstrap.min.css";
+// ............................
 import AllRoutes from "./Routes/AllRoutes";
 
 function App() {
   return (
     <div>
       <AllRoutes />
+      {/* <Navbar /> */}
     </div>
   );
 }
--- src/Components/HomePage/firHomeCaraousal.jsx
+++ src/Components/HomePage/firHomeCaraousal.jsx
@@ -1,15 +1,8 @@
-import React, { useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
 
-export default function FirCaraousal() {
-  const [index, setIndex] = useState(0);
-
-  const handleSelect = (selectedIndex, e) => {
-    setIndex(selectedIndex);
-  };
-
+function FirCaraousal() {
   return (
-    <Carousel activeIndex={index} onSelect={handleSelect}>
+    <Carousel>
       <Carousel.Item>
         <img
           className="d-block w-100"
@@ -83,3 +76,84 @@ export default function FirCaraousal() {
     </Carousel>
   );
 }
+
+export default FirCaraousal;
+
+/*
+export default function FirCaraousal() {
+
+return (
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://static1.lenskart.com/media/desktop/img/Dec22/5-Jan/1920x530(play-now)%20(1).gif"
+          alt="First slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://static1.lenskart.com/media/desktop/img/Dec22/15-Dec/1920x520_UPTO60_F1.gif"
+          alt="Second slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://static1.lenskart.com/media/desktop/img/Dec22/new-collection/desktop.gif"
+          alt="Third slide"
+        />
+      </Carousel.Item>{" "}
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://static1.lenskart.com/media/desktop/img/Dec22/1-Dec/Homepage-Banner-web.gif"
+          alt="Third slide"
+        />
+      </Carousel.Item>{" "}
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://static1.lenskart.com/media/desktop/img/Nov22/LK-Airflex-desktop-banner.gif"
+          alt="Third slide"
+        />
+      </Carousel.Item>{" "}
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://static1.lenskart.com/media/desktop/img/Nov22/tinted/launch-desk.gif"
+          alt="Third slide"
+        />
+      </Carousel.Item>{" "}
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://static1.lenskart.com/media/desktop/img/Oct22/kiara/Refresh-Banner-Web.gif"
+          alt="Third slide"
+        />
+      </Carousel.Item>{" "}
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://static1.lenskart.com/media/desktop/img/Nov22/mens-edit/desktop.jpg"
+          alt="Third slide"
+        />
+      </Carousel.Item>{" "}
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://static1.lenskart.com/media/desktop/img/Jan23/23jan/hooper/hooper-web.jpg"
+          alt="Third slide"
+        />
+      </Carousel.Item>{" "}
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://static1.lenskart.com/media/desktop/img/Nov22/trans1.gif"
+          alt="Third slide"
+        />
+      </Carousel.Item>
+    </Carousel>
+  ); 
+*/

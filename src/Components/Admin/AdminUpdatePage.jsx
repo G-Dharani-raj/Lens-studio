f724534185d97717949b75c62ea374cd09f88a75
G-Dharani-raj <105777295+G-Dharani-raj@users.noreply.github.com>
2023-01-20 12:13:04 +0530

    conflicts merged

---
+++ src/Components/Admin/AdminUpdatePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AdminNav from "./AdminNavbar/AdminNav";
+import AdminUpdateForm from "./AdminProducts/AdminUpdateForm";
+
+const AdminUpdatePage = () => {
+	return (
+		<>
+			<AdminNav />
+			<AdminUpdateForm />
+		</>
+	);
+};
+
+export default AdminUpdatePage;

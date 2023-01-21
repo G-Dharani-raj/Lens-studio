b7730f8a6e52c3aa995ebeedb07f46f9e95f4227
BaZuKaGanesh1 <ganesh12345bahire@gmail.com>
2023-01-21 15:56:24 +0530

    navbar fised

--- src/App.js
+++ src/App.js
@@ -5,15 +5,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // ............................
 import AllRoutes from "./Routes/AllRoutes";
 
-
 function App() {
-
-	return (
-		<div>
-			<AllRoutes />
-		</div>
-	);
-
+  return (
+    <div>
+      {/* <AllRoutes /> */}
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
